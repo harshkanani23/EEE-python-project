c6ae3c37d0a2a48506d179acd844018ab0ff809d
Jainam Desai <60807938+th3c0d3br34ker@users.noreply.github.com>
2020-03-24 12:31:13 +0530

    Check README.md
    Please get back after running the Restructured Code

---
+++ ElectricityBillAssumptionMain.py
@@ -0,0 +1,105 @@
+# Importing required modules.
+from os import system
+try:
+    from UtilityCore import getData, printDataFrame, writeToFile, createWorkbook
+except ModuleNotFoundError:
+    print("UtilityCore.py not found!")   
+
+# File where data will be stored.
+filename = 'ElectricityBill.xls'
+
+# Import relevant data from .json files.
+brandsdevices = getData('BrandDevices.json')
+state_units = getData('StateUnits.json')
+
+def start():
+  print('**************** WELCOME ****************')
+  # Create a workbook to work with.
+  wb = createWorkbook()
+  
+  # Add sheet to Workbook.
+  sheet1 = wb.add_sheet('Sheet 1')
+  
+  # For which project you want to estimate a electricity bill
+
+  print("\nEnter the name of project from [HOME], [COMMERCIAL], [SCHOOL], [COLLEGE], [SHOPPING MALL]")
+
+  project = input("\nEnter the name of project: ").lower()
+  state = input('\nEnter the name of state: ').lower()
+
+  if project == 'home':
+    bed = input("Enter the total number of BHK: ")
+  if project == 'commercial':
+    offices = input("Enter the total number of shops: ")
+  if project == 'school':
+    class_room = input("Enter the total number of classrooms: ")
+  if project == 'college':
+    class_room1 = input("Enter the total number of classrooms: ") 
+  if project == 'Shopping Mall':
+    shops = input('Enter the total number of shops: ')
+
+
+  # Total number os devices
+  total_devices = input("Enter Total Number of Devices you want to add: ")
+
+  # Storing heading in excel
+  sheet1.write(0, 1, 'Brand')
+  sheet1.write(0, 0, 'Device')
+  sheet1.write(0, 2, 'Quantity')
+  sheet1.write(0, 3, 'Voltage')
+  sheet1.write(0, 4, 'Current')
+  sheet1.write(0, 5, 'Power')
+  sheet1.write(0, 6, 'Hour/day')
+  sheet1.write(0, 7, 'Unit')
+  sheet1.write(0, 8, 'Total bill')
+  sheet1.write(0, 9, 'Remarks')               # remarks is for optimization of bill by using less
+                                              # consuming device suggested by program
+
+
+
+  # Algorithm to calculate optimal devices.
+  for i in range(int(total_devices)):
+    _name = input("Enter the name of device: ")
+    device_name = sheet1.write(i+1, 0, _name )
+    _name = input("Enter the name of brand: ")
+    brand = sheet1.write(i+1, 1, _name )
+    l = brandsdevices['devices']
+    if _name in l and _name in brandsdevices[_name]:
+      v = _name + '_vol'
+      current = _name + '_crr'
+      index = brandsdevices[_name].index(_name)
+      V = brandsdevices[v][index]
+      I = brandsdevices[current][index]
+    else:
+      V = input('Enter the voltage of device:')
+      I = input('Enter the current of device:')
+
+    qua = input("Enter the Quantity: ")
+    quantity = sheet1.write(i+1, 2, qua)
+    vlt = sheet1.write(i + 1, 3, V)
+    crt = sheet1.write(i + 1, 4, I)
+    usage = input('Enter your usage as hour/day: ')
+    use = sheet1.write(i+1, 6, usage)
+    power = int(V) * int(I)
+    pwr = sheet1.write(i+1, 5, power)
+    units = (power * int(usage) * 30 * int(qua)) / 1000
+    total_units = sheet1.write(i+1, 7, units)
+    bill = units * float(str(state_units[state]))
+    total_bill = sheet1.write(i+1, 8, bill)
+    li = brandsdevices[v]
+    v1 = li.index(min(li))
+    sheet1.write(i+1, 9, brandsdevices[_name][v1])
+
+
+  # Saving data in excel
+  writeToFile(wb, filename)
+  #wb.save('xlwt ')
+
+  # Print data in python emulator.
+  printDataFrame(filename)
+
+if "__name__" == "__main__":
+  system('cls')
+  start()
+
+
---
+++ UtilityCore.py
@@ -0,0 +1,56 @@
+from traceback import print_exc
+# Returns a dictinary of data of devices and brands.
+# Note: Data is tentative for project purpose.
+
+def getData(filename):
+    try:
+        # Import relevant modules.
+        from json import loads
+        data = ""
+        with open(filename, 'r') as file:
+            data = loads(file)
+        return data
+    except FileNotFoundError:
+        print("File not found!")
+    except :
+        print("Unable to load data!")
+        print_exc()
+
+def printDataFrame(filename):
+    try:
+        # Import relevant modules to read the excel file.
+        from pandas import read_excel
+        command = 'xlwt '+filename
+        print(read_excel(command))
+    except ModuleNotFoundError:
+        print("Pandas not  Installed!")
+    except FileNotFoundError:
+        print("File doesn't exist! \nCreate file first!") 
+    except:
+        print_exc()
+    finally:
+        print("Unable to print data from Excel file.")       
+
+def writeToFile(wb, filename):
+    try:
+        command = 'xlwt '+filename
+        from xlwt import Workbook
+        wb.save(command)
+    except ModuleNotFoundError:
+        print("xlwt not installed!")
+    except:
+        print_exc()
+    finally:
+        print("Unable to save!")
+
+def createWorkbook():
+    try:
+        # xlwt package is used for connection of excel and data entered.
+        from xlwt import Workbook
+        return Workbook()
+    except ModuleNotFoundError:
+        print("xlwt not installed!")
+    except:
+        print_exc()
+    finally:
+        print("Unable to create Workbook")
